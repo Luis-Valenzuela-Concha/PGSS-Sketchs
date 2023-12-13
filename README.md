2d4a696c91579a7c68880ce8e4af01900c0f0c32
Luis-Valenzuela-Concha <lvalenzuel2020@udec.cl>
2023-12-13 17:35:45 -0300

    terminado

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # PGSS-Sketchs
+Proyecto semestral de la asignatura Tópicos en Manejo de Grandes Vólumenes de Datos
 
 ## Compilación y ejecución
 g++ hashes/*.cpp PGSS_BDH.cpp main.cpp -o main.out && ./main.out 
--- test.cpp
+++ test.cpp
@@ -93,9 +93,9 @@ int cantidadAnomalias(vector<vector<string>> conexion){
 }
 
 int main(){
-    int time = 262144;
+    int time = 262144*2*2;
     //vector<vector<string>> fileNoTimeStamp = copiarArchivoSinTimestamp("datasets/chicago2015_withdata.txt",time);
-    //vector<vector<string>> file = copiarArchivo("datasets/chicago2015_withdata.txt",time);
+    vector<vector<string>> file = copiarArchivo("datasets/chicago2015_withdata.txt",time);
 
     /*unordered_map<string, vector<vector<string>>> separated;
     vector<vector<vector<string>>> conexiones;
@@ -111,11 +111,26 @@ int main(){
     }*/
 
     int m = 1000; int T = time;
-    int k = 5;
-    cout << "k: " << k << endl;
+    int k = 3;
     PGSS_BDH sketch (m,T,k);
-    cout << "size sketch: " <<sketch.size_in_bytes() << endl;
-
+    int i = 1;
+    for(vector<string> linea : file){
+        sketch.update(stoi(linea[4]),stoi(linea[5]),stoi(linea[6]),i);
+        i++;
+    }
+    int rep = 10;
+    freopen("time2.txt","w",stdout);
+    for(int i = 100000; i < time ; i+=100000){
+        auto tiempo_consulta = 0;
+        for(int r = 0; r<rep; r++){
+            auto start = chrono::high_resolution_clock::now();
+            vector<pair<int,int>> anomalias = sketch.find_anomalia(stoi(file[0][4]),stoi(file[0][5]),1,i,2);
+            auto finish = chrono::high_resolution_clock::now();
+            tiempo_consulta += chrono::duration_cast<chrono::milliseconds> (finish - start).count();
+        }
+        tiempo_consulta = tiempo_consulta/rep;
+        cout << "Tiempo de consulta para intervalo de " << i << " datos: " << tiempo_consulta << " ms" << endl << endl;
+    }
 
     /*freopen("test.txt","w",stdout);
     cout << "Calculo de ERM" << endl;
