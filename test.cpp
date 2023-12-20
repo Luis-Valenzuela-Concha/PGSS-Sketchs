68640e6c5661711708624f9e00479cef17243a6a
Luis-Valenzuela-Concha <lvalenzuel2020@udec.cl>
2023-12-20 13:58:31 -0300

    update test.cpp

--- test.cpp
+++ test.cpp
@@ -71,58 +71,99 @@ vector<vector<string>> copiarArchivo(string nombreArchivo, int n){
     return vec;
 }
 
-int cantidadAnomalias(vector<vector<string>> conexion){
-    int anomalias = 0;
+vector<vector<string>> return_anomalias(vector<vector<string>> conexion){
+    vector<vector<string>> anomalias;
     int promedio = 0;
     for(vector<string> linea : conexion){
-        promedio += stoi(linea[2]);
+        promedio += stoi(linea[6]);
     }
     promedio = promedio/conexion.size();
     int desviacion = 0;
     for(vector<string> linea : conexion){
-        desviacion += pow(stoi(linea[2]) - promedio,2);
+        desviacion += pow(stoi(linea[6]) - promedio,2);
     }
     desviacion = sqrt(desviacion/(conexion.size()));
 
     for(vector<string> linea : conexion){
-        if(stoi(linea[2]) < promedio - 1*desviacion || stoi(linea[2]) > promedio + 1*desviacion){
-            anomalias++;
+        if(stoi(linea[6]) < promedio - 1*desviacion || stoi(linea[6]) > promedio + 1*desviacion){
+            anomalias.push_back(linea);
         }
     }
     return anomalias;
 }
 
 int main(){
-    int time = 262144*2;
-    //vector<vector<string>> fileNoTimeStamp = copiarArchivoSinTimestamp("datasets/chicago2015_withdata.txt",time);
+    int time = 262144/2/2;
     vector<vector<string>> file = copiarArchivo("datasets/chicago2015_withdata.txt",time);
-
-    /*unordered_map<string, vector<vector<string>>> separated;
-    vector<vector<vector<string>>> conexiones;
-    for (const auto& vec : fileNoTimeStamp) {
-        separated[vec[1]].push_back(vec);
+    
+    unordered_map<string, vector<vector<string>>> separated;
+    for(vector<string> linea : file){
+        string key = linea[4] + "_" + linea[5]; // Combina la primera y la segunda IP para crear la clave
+        separated[key].push_back(linea);
     }
-    for (const auto& vec : separated) {
+    //freopen("conexiones.txt","w",stdout);
+    vector<vector<vector<string>>> separados;
+    vector<pair<string, vector<vector<string>>>> conexiones(separated.begin(), separated.end());
+    for(auto it = separated.begin(); it != separated.end(); it++){
         vector<vector<string>> conexion;
-        for (const auto& vec2 : vec.second) {
-            conexion.push_back(vec2);
+        for(const auto& linea : it->second){
+            conexion.push_back(linea);
         }
-        conexiones.push_back(conexion);
+        separados.push_back(conexion);
+    }
+
+    vector<vector<string>> anomalias_reales;
+    for(vector<vector<string>> conexion : separados){
+        vector<vector<string>> anomalias_conexion = return_anomalias(conexion);
+        for(vector<string> linea : anomalias_conexion){
+            anomalias_reales.push_back(linea);
+        }
+    }
+    /*cout << "Anomalias reales:" << endl;
+    for(vector<string> linea : anomalias_reales){
+        cout << linea[0] << " " << linea[1] << " " << linea[2] << " " << linea[3] << " " << linea[4] << " " << linea[5] << " " << linea[6] << endl;
     }*/
 
-    int m = 1000; int T = time;
-    int k = 5;
+
+    int m = 1000; int T = time; int k = 3;
     PGSS_BDH sketch (m,T,k);
+
     int i = 1;
     for(vector<string> linea : file){
         sketch.update(stoi(linea[4]),stoi(linea[5]),stoi(linea[6]),i);
         i++;
     }
+    vector<vector<string>> anomalias_estimadas;
+    for (vector<vector<string>> conexion : separados){
+        vector<pair<int,int>> anomalias = sketch.find_anomalia(stoi(conexion[0][4]),stoi(conexion[0][5]),1,time,1);
+        for(pair<int,int> anomalia : anomalias){
+            anomalias_estimadas.push_back(file[anomalia.first-1]);
+        }
+    }
+    /*cout << endl <<"Anomalias estimadas: " << endl;
+    for(vector<string> linea : anomalias_estimadas){
+        cout << linea[0] << " " << linea[1] << " " << linea[2] << " " << linea[3] << " " << linea[4] << " " << linea[5] << " " << linea[6] << endl;
+    }*/
 
-    freopen("size2.txt","w",stdout);
-    cout << "Calculo de tamaño" << endl;
-    cout << "Cantidad de conexiones: " << file.size() << endl << endl;
-    cout << "Tamaño de la estructura: " << sketch.size_in_bytes() << " bytes" << endl << endl;
+    int cantidad_anomalias_reales = anomalias_reales.size();
+    int cantidad_anomalias_estimadas = anomalias_estimadas.size();
+    cout << endl << "Cantidad de anomalias reales: " << cantidad_anomalias_reales << endl;
+    cout << "Cantidad de anomalias estimadas: " << cantidad_anomalias_estimadas << endl;
+
+
+    /*freopen("size.txt","w",stdout);
+    for (int k=2;k<=5;k++){
+        PGSS_BDH sketch (m,T,k);
+        int i = 1;
+        for(vector<string> linea : file){
+            sketch.update(stoi(linea[4]),stoi(linea[5]),stoi(linea[6]),i);
+            i++;
+        }
+
+        cout << "Funciones hash: " << k << endl;
+        cout << "Calculo de tamaño" << endl;
+        cout << "Tamaño de la estructura: " << sketch.size_in_bytes() << " bytes" << endl << endl;
+    }*/
 
     /*int rep = 10;
     freopen("size2.txt","w",stdout);
